705c079a6b614bad9108439549f6533b0675aedb
Bernardo Schmiedel Raymundo <98135145+dizzi-171@users.noreply.github.com>
2025-06-12 17:41:02 -0300

    Update setup_yolo.sh

--- setup_yolo.sh
+++ setup_yolo.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 echo "Criando ambiente virtual 'yolo-env'..."
 python3 -m venv yolo-env
-
 echo "Ambiente criado."
-
 echo "Ativando ambiente..."
 source yolo-env/bin/activate
-
 echo "Instalando dependências do requirements.txt..."
 pip install --upgrade pip
 pip install -r requirements.txt
-
 echo "Instalação concluída!"
 echo "Para ativar o ambiente manualmente depois, use: source yolo-env/bin/activate"
